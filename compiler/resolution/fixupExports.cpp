06caf3fbc552002319c29d6ec54c416a76202bc5
Brad Chamberlain <bradc@cray.com>
2019-10-02 15:13:10 -0700

    Permit string arguments to exported functions to have `in` intent

--- compiler/resolution/fixupExports.cpp
+++ compiler/resolution/fixupExports.cpp
@@ -219,18 +219,6 @@ static bool validateFormalIntent(FnSymbol* fn, ArgSymbol* as) {
         }
         return false;
       }
-    } else if (t == dtString) {
-      // TODO: After resolution, have abstract intents been normalized?
-      if (tag != INTENT_CONST &&
-          tag != INTENT_CONST_REF &&
-          tag != INTENT_BLANK) {
-        SET_LINENO(fn);
-        USR_FATAL_CONT(as,  "Formal \'%s\' of type \'%s\' in exported routine "
-                       "\'%s\' may not have the %s",
-                       as->name, t->name(), fn->name,
-                       intentDescrString(tag));
-        return false;
-      }
     }
   }
 

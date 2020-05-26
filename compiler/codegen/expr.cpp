ccc21aa6fcc54bd83abb39a24e8429068f597b54
Brad Chamberlain <bradcray@users.noreply.github.com>
2020-05-26 12:29:38 -0700

    Remove assertion which is causing problems to see the impact

--- compiler/codegen/expr.cpp
+++ compiler/codegen/expr.cpp
@@ -5625,7 +5625,7 @@ static bool codegenIsSpecialPrimitive(BaseAST* target, Expr* e, GenRet& ret) {
       SymExpr* se = toSymExpr(call->get(2));
 
       // Invalid AST to use PRIM_GET_MEMBER with a ref field
-      INT_ASSERT(!call->get(2)->isRefOrWideRef());
+      //      INT_ASSERT(!call->get(2)->isRefOrWideRef());
 
       if (call->get(1)->typeInfo()->symbol->hasFlag(FLAG_WIDE_CLASS) ||
           call->get(1)->isWideRef()   ||
